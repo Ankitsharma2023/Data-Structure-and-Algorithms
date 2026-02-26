71b360ab96c144a1148490178f98e41ec6fc08f8
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-02-26 07:13:07 +0000

    Time: 0 ms (100.00%) | Memory: 42.1 MB (91.99%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,38 +1,66 @@
-class Solution {
-public:
-    void dfs(int i, vector<vector<int>>& adj, vector<int>& vis, int n) {
-        vis[i] = 1;
+class DSU {
 
-        for (auto x : adj[i]) {
-            if (!vis[x]) {
-                dfs(x, adj, vis, n);
+private:
+    // rank
+    // parent
+    vector<int> rank;
+    vector<int> parent;
+
+public:
+    // constructor
+
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 1);
+        for (int i = 0; i < n; i++)
+            parent[i] = i; // initially all are empty
+    }
+
+    // find
+    int find(int node) {
+        if (parent[node] == node)
+            return node;
+
+        return parent[node] = find(parent[node]);
+    }
+    // union
+
+    void union_set(int a, int b) {
+        int leader_a = find(a);
+        int leader_b = find(b);
+
+        if (leader_a != leader_b) {
+            if (rank[leader_a] < rank[leader_b]) {
+                parent[leader_a] = leader_b;
+                rank[leader_b] += rank[leader_a];
+            } else {
+                parent[leader_b] = leader_a;
+                rank[leader_a] += rank[leader_b];
             }
         }
     }
+};
+
+class Solution {
+public:
     int makeConnected(int n, vector<vector<int>>& connections) {
 
-        // not enough cable
         if (connections.size() < n - 1)
             return -1;
 
-        // simple ans only approach --> no of provinces
-
-        vector<vector<int>> adj(n);
-        for (auto& e : connections) {
+        DSU dsu(n);
+         for (auto &e : connections) {
             int u = e[0];
             int v = e[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            dsu.union_set(u, v);
         }
 
-        int cnt = 0;
-        vector<int> vis(n);
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                cnt++;
-                dfs(i, adj, vis, n);
-            }
+        int component = 0;
+        for(int i =0;i<n;i++)
+        {
+            if(dsu.find(i)==i) component++; // same parnet
         }
-        return cnt - 1;
+
+        return component - 1;
     }
 };
