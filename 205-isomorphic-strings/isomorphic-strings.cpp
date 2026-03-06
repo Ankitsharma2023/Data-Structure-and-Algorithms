248085f22ab757f40c05102124c34e4db923e809
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-03-06 09:47:49 +0000

    Time: 3 ms (39.72%) | Memory: 9.5 MB (7.27%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+    
+        //imp check 
+        if(s.size()!=t.size()) return false;
+
+        unordered_map<char, char>st;
+        
+        unordered_map<char,char>ts;
+
+        for(int i = 0;i<s.size();i++)
+        {
+            char a = s[i];
+            char b = t[i];
+
+            if(st.count(a) && st[a]!=b) return false;
+            if(ts.count(b) && ts[b]!=a) return false;
+            st[a] =b;
+            ts[b] = a;
+        }
+
+        return true;
+        
+    }
+};
