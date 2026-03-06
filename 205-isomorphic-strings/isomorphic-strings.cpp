15cc0dd0d6e5c29157f12626d804caf450e95175
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-03-06 09:48:14 +0000

    Time: 0 ms (100.00%) | Memory: 9.3 MB (80.75%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -13,7 +13,7 @@ public:
         {
             char a = s[i];
             char b = t[i];
-
+// count define that this char exist already ,not a new one 
             if(st.count(a) && st[a]!=b) return false;
             if(ts.count(b) && ts[b]!=a) return false;
             st[a] =b;
