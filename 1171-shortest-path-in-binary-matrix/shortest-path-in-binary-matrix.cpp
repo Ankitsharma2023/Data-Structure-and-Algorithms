63d48040002e0d977960a76504a1d9c1d3f2b26b
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-04-18 13:55:55 +0000

    Time: 11 ms (86.09%) | Memory: 26.1 MB (39.25%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
-            return -1;
         
-          if (n == 1)
+
+     
+
+        int n = grid.size(); int m = grid[0].size();
+
+            if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1)
+            return -1;
+
+            if (n == 1)
             return 1;
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
 
- 
-        // source as 0,0 and destination as n-1,n-1;
-        pair<int, int> source = {0, 0};
-        pair<int, int> dest = {n - 1, n - 1};
-        queue<pair<int, pair<int, int>>> q;
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        queue<pair<int,pair<int,int>>>q;
+//for better considering all scenario not this particular question
+         pair<int, int> source = {0, 0};
+        pair<int, int> dest = {n - 1, m - 1};
 
+        q.push({1,{source.first,source.second}});
         dist[source.first][source.second] = 1;
-        q.push({1, {source.first, source.second}});
 
-        // delta row and col
         int dr[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
         int dc[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-        while (!q.empty()) {
-            auto it = q.front();
+        while(!q.empty())
+        {
+            int step = q.front().first;
+            int r = q.front().second.first;
+            int c =q.front().second.second;
             q.pop();
+            for(int i =0;i<8;i++)
+            {
+                int neigr = r +dr[i];
+                int neigc = c+dc[i];
 
-            int dis = it.first;
-            // row and col
-            int r = it.second.first;
-            int c = it.second.second;
+                if(neigr>=0 && neigr<n && neigc>=0 && neigc<m && grid[neigr][neigc]==0 && step+1<dist[neigr][neigc])
+                {
+                    dist[neigr][neigc] = 1 +step;
 
-            for (int i = 0; i < 8; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
-                    grid[nr][nc] == 0 && dis + 1 < dist[nr][nc]) {
-                    dist[nr][nc] = 1 + dis;
-                    if (nr == dest.first && nc == dest.second)
-                        return dist[nr][nc];
-                    q.push({dist[nr][nc], {nr, nc}});
+    //to stop at destination
+    if(neigr==dest.first && neigc ==dest.second) return dist[neigr][neigc];
+                    q.push({dist[neigr][neigc],{neigr,neigc}});
                 }
             }
+            
         }
 
-        return -1;
+        return -1; 
     }
 };
