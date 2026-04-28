fc1fda6f92e47200247a2be017f05ad9f7c5f57f
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-04-28 05:36:11 +0000

    Time: 14 ms (35.42%) | Memory: 18.9 MB (6.36%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,34 @@
+// simple make root->path node and check upto their latest connection
+class Solution {
+public:
+    bool findpath(TreeNode* root, TreeNode* target, vector<TreeNode*>& path) {
+        if (root == nullptr)
+            return false;
+        path.push_back(root);
+
+        if (root == target)
+            return true;
+        ;
+        if (findpath(root->left, target, path) ||
+            findpath(root->right, target, path))
+            return true;
+        path.pop_back();
+        return false;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> pp;
+        vector<TreeNode*> pq;
+        findpath(root, p, pp);
+        findpath(root, q, pq);
+
+        // return type TreeNode
+        TreeNode* lca = nullptr;
+        for (int i = 0; i < min(pp.size(), pq.size()); i++) {
+            if (pp[i] == pq[i])
+                lca = pp[i];
+            else
+                break;
+        }
+        return lca;
+    }
+};
