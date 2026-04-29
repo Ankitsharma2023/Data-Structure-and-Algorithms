14b02accd965db13d19efc85933800b3b0debfe1
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-04-29 07:02:49 +0000

    Time: 0 ms (100.00%) | Memory: 23.1 MB (20.23%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,21 +1,24 @@
 
 class Solution {
 public:
-       int maxDepth(TreeNode* root) {
-        
-        if(root==nullptr) return 0;
-        //leaf node 
-        if(root->left ==NULL && root->right ==nullptr) return 1; 
-        int lh = maxDepth(root->left);
-        int rh = maxDepth(root->right);
-        
-        if(lh==-1 ||rh==-1) return -1;
-        if(abs(lh-rh)>1) return -1;
+    int maxDepth(TreeNode* root) {
+        // base case
+        if (root == NULL)
+            return 0;
+        if (root->left == NULL && root->right == NULL)
+            return 1; // leaf node
 
-        return 1+ max(lh,rh);
+            int lh = maxDepth(root->left); 
+            int rh =maxDepth(root->right);
+            if(lh ==-1 || rh==-1) return -1;
+
+        if (abs(lh - rh) > 1)
+            return -1;
+        return 1 + max(lh, rh);
+
     }
-
     bool isBalanced(TreeNode* root) {
-        return maxDepth(root)!=-1;
+
+         return maxDepth(root) != -1;
     }
 };
