dfc8513741725af8f37c4e05dfa34978a6afedac
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-05-16 13:22:17 +0000

    Time: 0 ms (100.00%) | Memory: 8.8 MB (5.78%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+
+        stack<char> s1;
+
+        for (char c : s) {
+
+            if (c == '#') {
+
+                if (!s1.empty()) {
+
+                    s1.pop();
+                }
+            } else {
+
+                s1.push(c);
+            }
+        }
+
+        stack<char> t1;
+
+        for (char c : t) {
+
+            if (c == '#') {
+
+                if (!t1.empty()) {
+
+                    t1.pop();
+                }
+            } else {
+
+                t1.push(c);
+            }
+        }
+
+        return s1 == t1;
+    }
+};
