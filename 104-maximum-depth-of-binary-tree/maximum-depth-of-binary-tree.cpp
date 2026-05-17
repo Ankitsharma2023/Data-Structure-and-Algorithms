c558322c6dc21a1d70bdce43a4504a04924753be
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-05-17 10:46:53 +0000

    Time: 0 ms (100.00%) | Memory: 19 MB (76.69%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -7,7 +7,7 @@ public:
 
         //base case
         if(root==nullptr) return 0;
-        if(root->left ==NULL && root->right ==NULL) return 1;
+        // if(root->left ==NULL && root->right ==NULL) return 1;
 
         int lh = maxDepth(root->left);
         int rh = maxDepth(root->right);
