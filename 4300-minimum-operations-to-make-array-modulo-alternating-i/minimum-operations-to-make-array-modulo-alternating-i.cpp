0e2a8729ebffaa7272d98f06cfe59568a827c91a
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-05-27 11:21:48 +0000

    Time: 238 ms (7.56%) | Memory: 24 MB (52.91%) - LeetSync

--- 4300-minimum-operations-to-make-array-modulo-alternating-i/minimum-operations-to-make-array-modulo-alternating-i.cpp
+++ 4300-minimum-operations-to-make-array-modulo-alternating-i/minimum-operations-to-make-array-modulo-alternating-i.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
 
-    // Function to calculate minimum operations
-    // needed to convert remainder 'a' into remainder 'b'
     int dist(int a, int b, int k) {
 
-        // Operations if we keep incrementing
+        // incre
         int inc = (b - a + k) % k;
 
-        // Operations if we keep decrementing
+        // decrem
         int dec = (a - b + k) % k;
 
-        // Take minimum of both directions
+        //  minimum 
         return min(inc, dec);
     }
 
@@ -19,41 +17,36 @@ public:
 
         int ans = INT_MAX;
 
-        // Try every possible remainder for:
-        // x -> even indices
-        // y -> odd indices
+
 
         for (int x = 0; x < k; x++) {
 
             for (int y = 0; y < k; y++) {
 
-                // x and y must be different
+               //same ko skip kro
                 if (x == y) continue;
 
                 int ops = 0;
 
-                // Traverse the array
                 for (int i = 0; i < nums.size(); i++) {
 
-                    // Current remainder of element
                     int rem = nums[i] % k;
 
                     // Even index
                     if (i % 2 == 0) {
 
-                        // Convert current remainder -> x
                         ops += dist(rem, x, k);
                     }
 
                     // Odd index
                     else {
 
-                        // Convert current remainder -> y
+                
                         ops += dist(rem, y, k);
                     }
                 }
 
-                // Take minimum operations among all pairs
+        
                 ans = min(ans, ops);
             }
         }
