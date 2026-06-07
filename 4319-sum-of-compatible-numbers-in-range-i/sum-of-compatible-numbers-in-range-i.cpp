913f8288fabfd11742d439ed970b8070a36e7982
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-06-07 07:53:17 +0000

    Time: 0 ms (100.00%) | Memory: 8.6 MB (75.02%) - LeetSync

--- 4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.cpp
+++ 4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.cpp
@@ -3,7 +3,7 @@ public:
     int sumOfGoodIntegers(int n, int k) {
         
         long long sum = 0;
-
+ //it mean n-k <= x <= n+k
         for(int x = max(1,n-k); x<=n+k;x++)
         {
             if((n&x)==0) sum+=x;
