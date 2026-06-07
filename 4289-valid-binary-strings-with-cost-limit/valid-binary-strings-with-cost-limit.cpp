267fe3befffa152e544485845757c2b1277d695a
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-06-07 10:29:14 +0000

    Time: 14 ms (70.43%) | Memory: 30.2 MB (78.07%) - LeetSync

--- 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.cpp
+++ 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
+
+    void solve(int idx,int n,int k,int cost,char prev,string &curr,vector<string>&ans){
+
+        if(cost>k) return;
+        
+        if(idx==n){
+            ans.push_back(curr);
+            return;
+        }
+
+        //take 0
+        curr.push_back('0');
+        solve(idx+1,n,k,cost,'0',curr,ans);
+        curr.pop_back();
+
+        //take 1
+        if(prev!='1' && cost+idx<=k){
+            curr.push_back('1');
+            solve(idx+1,n,k,cost+idx,'1',curr,ans);
+            curr.pop_back();
+        }
+
+        
+    }
     vector<string> generateValidStrings(int n, int k) {
-        
-
         vector<string>ans;
-
-        int total = 1<<n; // 2^3
-        
-        for(int num = 0 ; num<total ;num++) //loop every possible string 0 1  2 3 ......
-        {
-            string s = "";
-            
-            for(int i =n-1;i>=0;i--) // n= 3 -> i = 2,1,0
-            {
-                if(num & (1<<i)) // non zero bit is on , else off. hmm isme num ko = 101 , i =2,i=1,i= 0 //to i=2 ,0 on hoga ,left shift
-                {
-                    s+='1';
-                }
-                else
-                {
-                    s+='0';
-                }
-            }
-
-             bool valid = true;
-        int cost = 0;
-
-        for(int i = 0;i<n;i++)
-        {
-            //calculate kro cost
-            if(s[i]=='1') cost+=i;
-
-            if(i>0 && s[i]=='1' && s[i-1]=='1') valid = false;
-        }
-
-        if(valid && cost<=k) ans.push_back(s);
-
-        }
-
+        string curr="";
+        solve(0,n,k,0,'0',curr,ans);
         return ans;
-
-       
     }
 };
