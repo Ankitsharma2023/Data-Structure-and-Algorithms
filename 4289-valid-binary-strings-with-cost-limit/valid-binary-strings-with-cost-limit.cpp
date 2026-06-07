aa76b97518a6bf76806424e44ffec379a3498a77
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-06-07 08:13:37 +0000

    Time: 109 ms (13.60%) | Memory: 28.1 MB (98.05%) - LeetSync

---
+++ 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<string> generateValidStrings(int n, int k) {
+        
+
+        vector<string>ans;
+
+        int total = 1<<n; // 2^3
+        
+        for(int num = 0 ; num<total ;num++) //loop every possible string 0 1  2 3 ......
+        {
+            string s = "";
+            
+            for(int i =n-1;i>=0;i--) // n= 3 -> i = 2,1,0
+            {
+                if(num & (1<<i)) // non zero bit is on , else off. hmm isme num ko = 101 , i =2,i=1,i= 0 //to i=2 ,0 on hoga 
+                {
+                    s+='1';
+                }
+                else
+                {
+                    s+='0';
+                }
+            }
+
+             bool valid = true;
+        int cost = 0;
+
+        for(int i = 0;i<n;i++)
+        {
+            //calculate kro cost
+            if(s[i]=='1') cost+=i;
+
+            if(i>0 && s[i]=='1' && s[i-1]=='1') valid = false;
+        }
+
+        if(valid && cost<=k) ans.push_back(s);
+
+        }
+
+        return ans;
+
+       
+    }
+};
