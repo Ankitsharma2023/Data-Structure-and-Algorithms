77a2937550d9984c0e8ff9469b5b9e29352d0177
Ankit Kumar <152326291+Ankitsharma2023@users.noreply.github.com>
2026-06-28 13:38:36 +0000

    Time: 90 ms (77.45%) | Memory: 192.3 MB (92.22%) - LeetSync

--- 3724-filter-occupied-intervals/filter-occupied-intervals.cpp
+++ 3724-filter-occupied-intervals/filter-occupied-intervals.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> filterOccupiedIntervals(vector<vector<int>>& occupiedIntervals,
-                                                int freeStart, int freeEnd) {
+    // question to me :Merge all busy intervals first, then erase the free
+    // interval from them, and return the remaining busy intervals.
+    vector<vector<int>>
+    filterOccupiedIntervals(vector<vector<int>>& occupiedIntervals,
+                            int freeStart, int freeEnd) {
+
+        // merge interval
         sort(occupiedIntervals.begin(), occupiedIntervals.end());
 
         vector<vector<int>> merged;
 
-        // Merge overlapping or touching intervals
-        for (auto &it : occupiedIntervals) {
-            if (merged.empty() || 1LL * it[0] > merged.back()[1] + 1) {
+        // Merge overlapping aur  touching intervals
+        for (auto& it : occupiedIntervals) {
+
+            if (merged.empty()) {
                 merged.push_back(it);
+                continue;
+            }
+
+            auto& last = merged.back();
+
+            if (it[0] <= last[1] + 1) {
+                last[1] = max(last[1], it[1]);
             } else {
-                merged.back()[1] = max(merged.back()[1], it[1]);
+                merged.push_back(it);
             }
         }
+        // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+        // < -- -- -- -- -- --->< -- -- ---><-- -- -- -->**********
 
         vector<vector<int>> ans;
 
         // Remove the free interval
-        for (auto &it : merged) {
+        for (auto& it : merged) {
             long long l = it[0], r = it[1];
 
             // No overlap
